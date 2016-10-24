d45c590613151566cc10ad102a808cb66cc6bf25
Frank Xu <frank.xu@northbright.com>
2016-10-24 16:43:34 +0800

    Don't use underscores in Go names.

--- main.go
+++ main.go
@@ -11,13 +11,13 @@ import (
 )
 
 var (
-	old_str          = ""
-	new_str          = ""
-	xml_file         = ""
-	res_path         = "./" //  resource folder
-	overlay_res_path = ""   // overlay resource folder
-	is_overlay_mode  = false
-	manual           = `Manual:
+	oldStr         = ""
+	newStr         = ""
+	xmlFile        = ""
+	resPath        = "./" //  resource folder
+	overlayResPath = ""   // overlay resource folder
+	isOverlayMode  = false
+	manual         = `Manual:
 replace_android_str_res is a tool to replace strings in xml files(<string>xxxx</string>) with new strings under Android app resource path(xx/res/).
 It will search / replace all files for multi-language resources under /res. Ex: values-zh-rCN/strings.xml...
 There're 2 modes:
@@ -28,7 +28,7 @@ There're 2 modes:
 It will search <string>xxx</string> in original xml files and only copy the item which contains the string to be replaced to new xml files under overlay folder.
  
 Usage:
-replace_android_str_res -s <old_string> -n <new_string> -f <xml file> -r <Android app resource path> -o <Android app resource overlay path>
+replace_android_str_res -s <oldString> -n <newString> -f <xml file> -r <Android app resource path> -o <Android app resource overlay path>
 
 Ex:
 replace_android_str_res -s "Miracast" -n "Wireless Display" -f "miracast_strings.xml" -r ~/proj/packages/app/settings/res -o ~/proj/overlay/packages/app/settings/res
@@ -39,47 +39,47 @@ replace_android_str_res -s "Miracast" -n "Wireless Display" -f "miracast_strings
 It will find <string>xxx</string> item which contains the string to be replaced then overwrite the original xml files.
       
 Usage:
-replace_android_str_res -s <old_string> -n <new_string> -f <xml file> -r <Android app resource path>
+replace_android_str_res -s <oldString> -n <newString> -f <xml file> -r <Android app resource path>
 
 Ex:
 replace_android_str_res -s "Miracast" -n "Wireless Display" -f "miracast_strings.xml" -r ~/proj/packages/app/settings/res`
 )
 
 func main() {
-	flag.StringVar(&old_str, "s", "", "old string")
-	flag.StringVar(&new_str, "n", "", "new string")
-	flag.StringVar(&xml_file, "f", "", "xml file name without path. Ex string.xml")
-	flag.StringVar(&res_path, "r", "./", "Android app resource folder path. Ex: ~/proj/packages/apps/settings/res")
-	flag.StringVar(&overlay_res_path, "o", "", "Android app overlay resource folder path to output new xml files. Ex: ~/proj/overlay/packages/apps/settings/res")
+	flag.StringVar(&oldStr, "s", "", "old string")
+	flag.StringVar(&newStr, "n", "", "new string")
+	flag.StringVar(&xmlFile, "f", "", "xml file name without path. Ex string.xml")
+	flag.StringVar(&resPath, "r", "./", "Android app resource folder path. Ex: ~/proj/packages/apps/settings/res")
+	flag.StringVar(&overlayResPath, "o", "", "Android app overlay resource folder path to output new xml files. Ex: ~/proj/overlay/packages/apps/settings/res")
 
 	flag.Parse()
 
-	fmt.Println("old_str: " + old_str)
-	fmt.Println("new_str: " + new_str)
-	fmt.Println("xml_file: " + xml_file)
-	fmt.Println("res_path: " + res_path)
-	fmt.Println("overlay_res_path: " + overlay_res_path)
+	fmt.Println("oldStr: " + oldStr)
+	fmt.Println("newStr: " + newStr)
+	fmt.Println("xmlFile: " + xmlFile)
+	fmt.Println("resPath: " + resPath)
+	fmt.Println("overlayResPath: " + overlayResPath)
 
-	if len(old_str) == 0 || len(xml_file) == 0 {
+	if len(oldStr) == 0 || len(xmlFile) == 0 {
 		fmt.Println(manual)
 		return
 	}
 
-	if len(overlay_res_path) == 0 {
-		is_overlay_mode = false
+	if len(overlayResPath) == 0 {
+		isOverlayMode = false
 	} else {
-		is_overlay_mode = true
+		isOverlayMode = true
 	}
 
-	fmt.Printf("is_overlay_mode: %v\n", is_overlay_mode)
+	fmt.Printf("isOverlayMode: %v\n", isOverlayMode)
 
-	pattern := fmt.Sprintf(`.*/?res/(?P<folder>.*)/%s:\d.*:\s*(?P<item><string .*>.*</string>)`, xml_file)
+	pattern := fmt.Sprintf(`.*/?res/(?P<folder>.*)/%s:\d.*:\s*(?P<item><string .*>.*</string>)`, xmlFile)
 
 	// Step 1. Use find and grep to search "Miracast" related strings in miracast_strings.xml.
 
 	// use this method, we'll get 'exit status 123' error by xargs, see:
 	// http://lists.gnu.org/archive/html/help-gnu-emacs/2013-04/msg00340.html
-	cmd := fmt.Sprintf("find %s -name '%s' | xargs grep -e '<string .*>.*%s.*</string>' -rn --color", res_path, xml_file, old_str)
+	cmd := fmt.Sprintf("find %s -name '%s' | xargs grep -e '<string .*>.*%s.*</string>' -rn --color", resPath, xmlFile, oldStr)
 	fmt.Println(cmd)
 
 	// use this method, no 'exit status 123' error code. But it's much slower.
@@ -109,33 +109,33 @@ func main() {
 	stringMap := make(map[string][]string) // key: values folder name(Ex: values, values-zh-rCN). value: new string slice contains "Miracast"(Ex: <string name="xxxx">Miracast</string>.
 
 	// for overwirte mode
-	old_stringMap := make(map[string][]string) // key: value folder name(Ex: values, values-zh-rCn). value: old string item with the same index as new string slice in stringMap.
+	oldStringMap := make(map[string][]string) // key: value folder name(Ex: values, values-zh-rCn). value: old string item with the same index as new string slice in stringMap.
 
 	for i := 0; i < len(matchedItems); i++ {
 		folder := matchedItems[i][1]
 		item := matchedItems[i][2]
 
-		new_item := strings.Replace(item, old_str, new_str, -1) // replace string
-		stringMap[folder] = append(stringMap[folder], new_item)
-		if !is_overlay_mode {
-			old_stringMap[folder] = append(old_stringMap[folder], item) // record old string
+		newItem := strings.Replace(item, oldStr, newStr, -1) // replace string
+		stringMap[folder] = append(stringMap[folder], newItem)
+		if !isOverlayMode {
+			oldStringMap[folder] = append(oldStringMap[folder], item) // record old string
 		}
 	}
 
 	// Step 3. Create new miracast_strings.xml and values folder under Setttings overlay folder.
 	for k, v := range stringMap {
-		if is_overlay_mode {
+		if isOverlayMode {
 			// overlay mode
 			//fmt.Println(k)
-			folder := overlay_res_path + "/" + k
+			folder := overlayResPath + "/" + k
 			//fmt.Println(folder)
 			if err := os.MkdirAll(folder, os.ModeDir|os.ModePerm); err != nil {
 				fmt.Println(err)
 				return
 			}
 
-			file_name := folder + "/" + xml_file
-			f, err := os.Create(file_name)
+			fileName := folder + "/" + xmlFile
+			f, err := os.Create(fileName)
 			defer f.Close()
 			if err != nil {
 				fmt.Println(err)
@@ -156,24 +156,24 @@ func main() {
 			}
 		} else {
 			// overwrite mode
-			file_name := res_path + "/" + k + "/" + xml_file
-			//fmt.Println(file_name)
-			buf, err := ioutil.ReadFile(file_name)
+			fileName := resPath + "/" + k + "/" + xmlFile
+			//fmt.Println(fileName)
+			buf, err := ioutil.ReadFile(fileName)
 			if err != nil {
 				fmt.Println(err)
 				return
 			}
 
 			if len(buf) == 0 {
-				fmt.Println("empty file: " + file_name)
+				fmt.Println("empty file: " + fileName)
 				return
 			}
 
 			s := string(buf)
 			for i := 0; i < len(v); i++ {
-				s = strings.Replace(s, old_stringMap[k][i], v[i], -1) // replace old item with new item
+				s = strings.Replace(s, oldStringMap[k][i], v[i], -1) // replace old item with new item
 			}
-			if err := ioutil.WriteFile(file_name, []byte(s), os.ModePerm); err != nil {
+			if err := ioutil.WriteFile(fileName, []byte(s), os.ModePerm); err != nil {
 				fmt.Println(err)
 				return
 			}
